5a3c856d400e81e7504b17a5474bf4ef5cab8024
Bryan Zhang <johnhall@126.com>
2024-09-08 14:28:59 +0800

    Update model.py

--- model.py
+++ model.py
@@ -4,11 +4,13 @@ import torch
 class SimpleModel(torch.nn.Module):
     def __init__(self):
         super(SimpleModel, self).__init__()
-        self.fc1 = torch.nn.Linear(784, 128)
-        self.fc2 = torch.nn.Linear(128, 10)
+        self.fc1 = torch.nn.Linear(784, 512)
+        self.fc2 = torch.nn.Linear(512, 10)
+        self.dropout1 = torch.nn.Dropout(0.2)
 
     def forward(self, x):
         x = x.view(-1, 784)
         x = torch.relu(self.fc1(x))
+        x = self.dropout1(x)
         x = self.fc2(x)
         return x
